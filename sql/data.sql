92a2c57eec9552924c908f00d1aed989d19e83d0
omarbenzi <52929955+omarbenzi@users.noreply.github.com>
2020-03-08 16:31:30 -0400

    formatage

--- sql/data.sql
+++ sql/data.sql
@@ -1,32 +1,24 @@
 
-INSERT INTO `
-mydb`.`image
-`
+INSERT INTO `mydb`.`image`
 (`idimage`, `lien`) VALUES
 (NULL, 'lientTest1'),
 (NULL, 'lientTest2');
 
-INSERT INTO `
-mydb`.`ville
-`
+INSERT INTO `mydb`.`ville`
 (`idVille`, `nomVille`, `codePostale`) VALUES
-(NULL, 'Montreal', 'H2S3W2'),
+(NULL, 'Montreal', 'H2S3W2');
 
 
 
-INSERT INTO `
-mydb`.`categorie
-`
+INSERT INTO `mydb`.`categorie`
 (`idcategorie`, `nom`) VALUES
 (NULL, 'AUTO'),
 (NULL, 'MAISON');
 
 
-INSERT INTO `
-mydb`.`sous
--categorie`
+INSERT INTO `mydb`.`sous-categorie`
 (`idsous-categorie`, `nom`, `id_categorie`) VALUES
 (NULL, 'pieces', '1'),
 (NULL, 'roue', '1'),
 (NULL, 'cuisine', '2'),
-(NULL, 'jardin', '2')
+(NULL, 'jardin', '2');
