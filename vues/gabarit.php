459d1d7cc3a9cc28963b6267371f5d4804659667
omarbenzi <52929955+omarbenzi@users.noreply.github.com>
2020-04-18 16:45:02 -0400

    reference les fichier

--- vues/gabarit.php
+++ vues/gabarit.php
@@ -3,7 +3,14 @@
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="<?php echo str_replace("\\", "", Controleur::$base_uri) ?>/styles/styles.css">
+    <link rel="stylesheet" href="<?php echo str_replace("\\", "", Controleur::$base_uri) ?>/styles/basic-style.css">
+    <link rel="stylesheet" href="<?php echo str_replace("\\", "", Controleur::$base_uri) ?>/styles/normalize.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel="stylesheet"> <!-- google font -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <title><?php echo $titre ?></title>
 </head>
 
@@ -22,6 +29,7 @@
             <!-- contenu d'une vue spécifique -->
         </div>
         <footer>
+            <script type="text/javascript" src="<?php echo str_replace("\\", "", Controleur::$base_uri) ?>js/main.js"></script>
         </footer>
     </div>
 </body>
